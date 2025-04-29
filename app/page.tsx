a54b097aa3271c10f35276ca91c291851452d220
Ayush Chandekar <149964756+AyushChandekar@users.noreply.github.com>
2025-04-29 12:43:00 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -339,7 +339,7 @@ export default function Home() {
                   <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"></path>
                 </svg>
               </Link>
-              <Link href="https://www.instagram.com/gearup_287" className="text-muted-foreground hover:text-foreground">
+              <Link href="https://www.instagram.com/gearup_287" target="_blank" className="text-muted-foreground hover:text-foreground">
                 <span className="sr-only">Instagram</span>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
